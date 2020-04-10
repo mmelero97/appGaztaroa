05fb394dcab754cffa545a8e0d8491be275e104c
Marta <mmelero97@gmail.com>
2020-04-10 16:33:06 +0200

    Componentes funcionales en React Native

--- componentes/CalendarioComponent.js
+++ componentes/CalendarioComponent.js
@@ -12,6 +12,7 @@ function Calendario(props) {
                 title={item.nombre}
                 subtitle={item.descripcion}
                 hideChevron={true}
+                onPress={() => props.onPress(item.id)}
                 leftAvatar={{ source: require('./imagenes/40Años.png') }}
             />
         );
