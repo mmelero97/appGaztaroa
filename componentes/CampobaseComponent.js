9e6b375aa21cfe5239b9f2c622732a78aa9a9085
Marta <mmelero97@gmail.com>
2020-04-10 18:51:40 +0200

    Drawer Navigation

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Calendario from './CalendarioComponent';
+import Home from './HomeComponent';
 import DetalleExcursion from './DetalleExcursionComponent';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
 
 function CalendarioNavegador() {
     return (
@@ -36,6 +40,43 @@ function CalendarioNavegador() {
     );
 }
 
+function HomeNavegador() {
+    return (
+        <Stack.Navigator
+            initialRouteName="Home"
+            headerMode="screen"
+            screenOptions={{
+                headerTintColor: '#fff',
+                headerStyle: { backgroundColor: '#015afc' },
+                headerTitleStyle: { color: '#fff' },
+            }}
+        >
+            <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    title: 'Campo Base',
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+function DrawerNavegador() {
+    return (
+        <Drawer.Navigator
+            drawerStyle={{
+                backgroundColor: '#c2d3da',
+            }}
+            initialRouteName="Home"
+        >
+            <Drawer.Screen name="Home" component={HomeNavegador} />
+            <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
+        </Drawer.Navigator>
+    );
+}
+
+
 class Campobase extends Component {
 
     render() {
@@ -43,7 +84,7 @@ class Campobase extends Component {
         return (
             <NavigationContainer>
                 <View style={{ flex: 1 }}>
-                    <CalendarioNavegador />
+                    <DrawerNavegador />
                 </View>
             </NavigationContainer>
         );
